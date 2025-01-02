9cc07e3bc54af26796c8c8558a0462e9a55be999
khalil <khalilm22393@gmail.com>
2025-01-02 23:01:37 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\AddressController::class,"index"]);
-//Route::get('/addresses', [\App\Http\Controllers\AddressController::class,"index"]);
+
--- tests/Feature/khalilTest.php
+++ tests/Feature/khalilTest.php
@@ -8,14 +8,14 @@ use Tests\TestCase;
 
 class khalilTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
     public function test_example(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this->post('/api/region',['name'=>'reg2']);
+        $response->assertStatus(201);
 //        $response->assertSee('heelo');
     }
 }
