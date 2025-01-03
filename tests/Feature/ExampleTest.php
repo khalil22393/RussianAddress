de6084480931ab0311ed2aa012615c7431673aa6
khalil22393 <60979517+khalil22393@users.noreply.github.com>
2025-01-03 13:29:58 +0300

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         // try{
-        $response = $this->post('/regions',['name'=>'new region1']);
+        $response = $this->post('/api/region',['name'=>'new region1']);
 
         $response->assertStatus(201);
         // }catch(\Exception $e){
